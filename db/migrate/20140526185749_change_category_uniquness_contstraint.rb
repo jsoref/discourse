62d8afd696d0b64dfa6b4938dd1881c0b7cb7e29
Josh Soref <jsoref@users.noreply.github.com>
2021-04-19 21:51:58 -0400

    spelling: uniqueness
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- db/migrate/20140526185749_change_category_uniquness_contstraint.rb
+++ db/migrate/20140526185749_change_category_uniquness_contstraint.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class ChangeCategoryUniqunessConstraint < ActiveRecord::Migration[4.2]
+class ChangeCategoryUniquenessConstraint < ActiveRecord::Migration[4.2]
   def change
     remove_index :categories, name: 'index_categories_on_name'
     add_index :categories, [:parent_category_id, :name], unique: true
